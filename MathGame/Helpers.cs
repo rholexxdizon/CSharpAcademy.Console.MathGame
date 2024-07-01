279e40cac25c6aa45ee3ce1f061616df462c3545
rholexxdizon <dizonrholex02@gmail.com>
2024-07-01 15:06:13 +0800

    Added Menu and Helpers Class

---
+++ MathGame/Helpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGame
+{
+    internal class Helpers
+    {
+        internal static string GetName()
+        {
+            Console.WriteLine("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            while(string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("Name cannot be empty.");
+                Console.WriteLine("Please enter your name: ");
+                name = Console.ReadLine();
+            }
+            return name;
+        }
+    }
+}
--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,4 +1,7 @@
 ﻿
+using MathGame;
+
+
+var name = Helpers.GetName();
 var date = DateTime.UtcNow;
-
-
+var menu = new Menu();
